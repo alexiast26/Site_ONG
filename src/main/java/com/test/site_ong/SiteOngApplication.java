06bfcdb18b5e7cd13099d55f44a4f6592e710c59
Stoleru Alexia <alxiastoleru26@gmail.com>
2025-10-22 19:22:48 +0300

    initial commit

---
+++ src/main/java/com/test/site_ong/SiteOngApplication.java
@@ -0,0 +1,13 @@
+package com.test.site_ong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteOngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteOngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/site_ong/SiteOngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.site_ong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteOngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
