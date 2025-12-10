990ec450cf7026367c6bdf3c151f2a5c9246628e
Martinas Mihai <misu.martinas@gmail.com>
2025-12-10 13:16:03 +0200

    fixed image

--- Admin Page/adminpanel/src/main/java/com/example/adminpanel/config/WebConfig.java
+++ Admin Page/adminpanel/src/main/java/com/example/adminpanel/config/WebConfig.java
@@ -1,15 +1,25 @@
 package com.example.adminpanel.config;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${upload.path:uploads}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+        Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
+        String uploadPathStr = uploadPath.toUri().toString();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(uploadPathStr);
     }
 }
